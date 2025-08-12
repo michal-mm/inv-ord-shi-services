c836d1157c5512f6fcdde1278aa73034903da423
michal-mm <michal@Magic.local>
2025-08-12 16:03:14 +0200

    stub implementation of order service using spring boot (all generated using spring initializer)

---
+++ order-service/src/main/java/com/michal_Michal/Miotk/ois/orderservice/OrderInvetoryShippingUiApplication.java
@@ -0,0 +1,13 @@
+package com.michal_Michal.Miotk.ois.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInvetoryShippingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInvetoryShippingUiApplication.class, args);
+	}
+
+}
---
+++ order-service/src/test/java/com/michal_Michal/Miotk/ois/orderservice/OrderInvetoryShippingUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michal_Michal.Miotk.ois.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInvetoryShippingUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
