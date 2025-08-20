c98bd7066a504230eb1890162ccc2ec893dc52d6
michal-mm <michal@Magic.local>
2025-08-20 11:59:18 +0200

    adjusting SQL insert queries to have also item_prize for the initial data set

--- order-service/src/main/resources/data.sql
+++ order-service/src/main/resources/data.sql
@@ -8,9 +8,9 @@ CREATE TABLE orders
 );
 */
 
-INSERT INTO orders (order_id, item_id, order_name, quantity) 
-VALUES ('005fd704-8a42-456a-90aa-8e4168eb3375', '005fd704-8a42-456a-90aa-8e4168eb3375', 'Order number zero', '5');
+INSERT INTO orders (order_id, item_id, order_name, quantity, item_price) 
+VALUES ('005fd704-8a42-456a-90aa-8e4168eb3375', '005fd704-8a42-456a-90aa-8e4168eb3375', 'Order number zero', '5', '200');
 
-INSERT INTO orders (order_id, item_id, order_name, quantity) 
-VALUES ('005fd704-8a42-456a-90aa-8e4168eb3376', '005fd704-8a42-456a-90aa-8e4168eb3375', 'Order number zero-B', '15');
+INSERT INTO orders (order_id, item_id, order_name, quantity, item_price) 
+VALUES ('005fd704-8a42-456a-90aa-8e4168eb3376', '005fd704-8a42-456a-90aa-8e4168eb3375', 'Order number zero-B', '15', '200');
 
