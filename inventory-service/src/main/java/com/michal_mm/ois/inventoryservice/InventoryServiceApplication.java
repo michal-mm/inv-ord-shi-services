4c5acc9adac606aa8c06ef2003fa0b64ff3d3386
michal-mm <michal@Mac.lan>
2025-09-15 10:06:41 +0200

    adding inventory service base implementation generated with start.spring.io

---
+++ inventory-service/src/main/java/com/michal_mm/ois/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.michal_mm.ois.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
