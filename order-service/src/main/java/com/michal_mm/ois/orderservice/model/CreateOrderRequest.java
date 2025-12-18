7ffbd38b43a6344e2b5c7cc5e6f0a6ac45640499
michal-mm <michal.miotk@gmail.com>
2025-12-18 14:47:56 +0100

    Removing old TODO comment

--- order-service/src/main/java/com/michal_mm/ois/orderservice/model/CreateOrderRequest.java
+++ order-service/src/main/java/com/michal_mm/ois/orderservice/model/CreateOrderRequest.java
@@ -2,7 +2,6 @@ package com.michal_mm.ois.orderservice.model;
 
 import java.util.UUID;
 
-// TODO - input validation rules missing!
 public record CreateOrderRequest(
 	UUID itemId,
 	String orderName,
